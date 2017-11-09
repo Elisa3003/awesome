c9998fe80c4d03b26ec5379479f153dc6969ed6c
Elisa <elisatqy@gmail.com>
2017-11-09 14:18:02 +0000

    add heading

--- clearData.php
+++ clearData.php
@@ -6,8 +6,8 @@
     <body>
         <?php
             $myfile=fopen("data.txt","w");
+            fwrite($myfile, "|time/s|speed/steps/s|total steps"."\n");
             fclose($myfile);
-            
         ?>
     </body>
 </html>
