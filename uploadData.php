c1f2885c99f71103bbe5c669f9de6e9901da426b
Elisa <elisatqy@gmail.com>
2017-11-09 11:24:48 +0000

    add file write

--- uploadData.php
+++ uploadData.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title></title>
+    <title></title>
 </head>
-<body>
-<?php
-echo $_GET['v1'].$_GET['v2'];
-?>
-</body>
+    <body>
+        <?php
+            echo $_GET['v1'].$_GET['v2'].$_GET['v3'];
+            $myfile=fopen("data.txt","a+");
+            fwrite($myfile, $_GET['v1']."|".$_GET['v2']."|".$_GET['v3']."\n");
+        ?>
+    </body>
 </html>
