02bdd99cbd190cdc1fb18496cdfbc8122934b2a0
Elisa <elisatqy@gmail.com>
2017-11-09 14:12:44 +0000

    change variable name

--- uploadData.php
+++ uploadData.php
@@ -5,9 +5,9 @@
 </head>
     <body>
         <?php
-            echo $_GET['v1'].$_GET['v2'].$_GET['v3'];
+            echo $_GET['time'].$_GET['speed'].$_GET['steps'];
             $myfile=fopen("data.txt","a+");
-            fwrite($myfile, "|".$_GET['v1']."|".$_GET['v2']."|".$_GET['v3']."\n");
+            fwrite($myfile, "|".$_GET['time']."|".$_GET['speed']."|".$_GET['steps']."\n");
             fclose($myfile);
         ?>
     </body>
