4d880f97c1eed560ecf0c27043a45bd458f6e925
Elisa <elisatqy@gmail.com>
2017-11-09 11:27:29 +0000

    add |

--- uploadData.php
+++ uploadData.php
@@ -7,7 +7,7 @@
         <?php
             echo $_GET['v1'].$_GET['v2'].$_GET['v3'];
             $myfile=fopen("data.txt","a+");
-            fwrite($myfile, $_GET['v1']."|".$_GET['v2']."|".$_GET['v3']."\n");
+            fwrite($myfile, "|".$_GET['v1']."|".$_GET['v2']."|".$_GET['v3']."\n");
         ?>
     </body>
 </html>
